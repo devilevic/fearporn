6d120de34af6b54c321ec542ffca9bf6536057ef
I. D <i.d.@Is-MacBook-Pro.local>
2025-12-17 17:12:30 +0100

    Use Render PORT env var

--- server.js
+++ server.js
@@ -173,4 +173,8 @@ return (
 }
 
 
-app.listen(3000, () => console.log("Local server: http://localhost:3000"));
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
